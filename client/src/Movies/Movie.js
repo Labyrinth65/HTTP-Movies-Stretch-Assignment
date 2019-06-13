1502a597ceab1ac139adc97c024ce06108271ac6
Ang Xu <angxu1234@gmail.com>
2019-06-13 18:55:24 -0500

    parts 1 and 2 complete

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,46 +1,55 @@
-import React from 'react';
-import axios from 'axios';
-import MovieCard from './MovieCard';
+import React from "react";
+import axios from "axios";
+import MovieCard from "./MovieCard";
 export default class Movie extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movie: null
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			movie: null
+		};
+	}
 
-  componentDidMount() {
-    this.fetchMovie(this.props.match.params.id);
-  }
+	componentDidMount() {
+		this.fetchMovie(this.props.match.params.id);
+	}
 
-  componentWillReceiveProps(newProps) {
-    if (this.props.match.params.id !== newProps.match.params.id) {
-      this.fetchMovie(newProps.match.params.id);
-    }
-  }
+	componentWillReceiveProps(newProps) {
+		if (this.props.match.params.id !== newProps.match.params.id) {
+			this.fetchMovie(newProps.match.params.id);
+		}
+	}
 
-  fetchMovie = id => {
-    // this function needs to fire off a get request to localhost:5000/api/movies/:id
-    // note that the id is dynamic.
-  };
+	fetchMovie = id => {
+		// this function needs to fire off a get request to localhost:5000/api/movies/:id
+		// note that the id is dynamic.
+		axios
+			.get(`http://localhost:5000/api/movies/${id}`)
+			.then(response => {
+				this.setState({ movie: response.data });
+				console.log(response);
+			})
+			.catch(err => {
+				this.setState({ error: err.message });
+			});
+	};
 
-  saveMovie = () => {
-    const addToSavedList = this.props.addToSavedList;
-    addToSavedList(this.state.movie);
-  };
+	saveMovie = () => {
+		const addToSavedList = this.props.addToSavedList;
+		addToSavedList(this.state.movie);
+	};
 
-  render() {
-    if (!this.state.movie) {
-      return <div>Loading movie information...</div>;
-    }
+	render() {
+		if (!this.state.movie) {
+			return <div>Loading movie information...</div>;
+		}
 
-    return (
-      <div className="save-wrapper">
-        <MovieCard movie={this.state.movie} />
-        <div className="save-button" onClick={this.saveMovie}>
-          Save
-        </div>
-      </div>
-    );
-  }
+		return (
+			<div className="save-wrapper">
+				<MovieCard movie={this.state.movie} />
+				<div className="save-button" onClick={this.saveMovie}>
+					Save
+				</div>
+			</div>
+		);
+	}
 }
